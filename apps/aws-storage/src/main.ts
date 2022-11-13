6be067f56bbc50c647b31d25808976cb86ca86e4
Myat Kyaw Tun <myatktun2k@gmail.com>
2022-11-14 01:12:41 +0630

    aws-storage: add service to get data from s3
    src/main.ts : get object from s3 bucket based on file name from the
                  request and pipe the object's body to the response

---
+++ apps/aws-storage/src/main.ts
@@ -0,0 +1,68 @@
+import express from 'express'
+import { S3Client, GetObjectCommand, GetObjectCommandInput } from '@aws-sdk/client-s3'
+import { Readable } from 'stream'
+
+const app = express()
+
+if (!process.env.S3_REGION) {
+    throw new Error("Environment variable S3_REGION not set")
+}
+
+if (!process.env.PORT) {
+    throw new Error("Environment variable PORT not set")
+}
+
+if (!process.env.S3_BUCKET) {
+    throw new Error("Environment variable S3_BUCKET not set")
+}
+
+const PORT = process.env.PORT
+const S3_REGION = process.env.S3_REGION
+const S3_BUCKET = process.env.S3_BUCKET
+const s3Client = new S3Client({ region: S3_REGION })
+
+const getObject = async (params: GetObjectCommandInput) => {
+    try {
+        const command = new GetObjectCommand(params)
+        const object = await s3Client.send(command)
+        return object
+    } catch (error) {
+        console.log(error)
+        return new Error(<string>error)
+    }
+}
+
+app.get("/video", async (req, res) => {
+    if (typeof req.query.path !== 'string') {
+        res.status(404).send("Error")
+        return
+    }
+
+    const videoPath = req.query.path
+    console.log(`Streaming video from path ${videoPath}`)
+
+    const params: GetObjectCommandInput = {
+        Bucket: S3_BUCKET,
+        Key: videoPath
+    }
+
+    const object = await getObject(params)
+
+    if (object instanceof Error) {
+        res.status(404).send("Error")
+        return
+    }
+
+    res.writeHead(200, {
+        "Content-Length": object.ContentLength,
+        "Content-Type": object.ContentType,
+    })
+
+    if (object.Body instanceof Readable) {
+        object.Body.pipe(res)
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`AWS Storage service on port ${PORT}`)
+})
