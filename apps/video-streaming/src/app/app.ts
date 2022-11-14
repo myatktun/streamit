e59fa3b2e50b55e414b1a0fd485744ebad149409
Myat Kyaw Tun <myatktun2k@gmail.com>
2022-11-15 00:08:08 +0630

    video-streaming: find video file name in database
    src/app/app.ts : connect to database and find the video file name based
                     on 'id' by 'req' and pass the file name to
                     'forwardRequest'
    src/main.ts    : catch errors

--- apps/video-streaming/src/app/app.ts
+++ apps/video-streaming/src/app/app.ts
@@ -1,19 +1,62 @@
 import express from "express"
 import { request } from 'http'
+import { MongoClient, ObjectId } from 'mongodb'
 
 const app = express()
 
+if (!process.env.VIDEO_STORAGE_HOST) {
+    throw new Error("Environment variable VIDEO_STORAGE_HOST not specified")
+}
+
+if (!process.env.VIDEO_STORAGE_PORT) {
+    throw new Error("Environment variable VIDEO_STORAGE_PORT not specified")
+}
+
+if (!process.env.DBHOST) {
+    throw new Error("Environment variable DBHOST not specified")
+}
+
+if (!process.env.DBNAME) {
+    throw new Error("Environment variable DBNAME not specified")
+}
+
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST
 const VIDEO_STORAGE_PORT = process.env.VIDEO_STORAGE_PORT
+const DBHOST = process.env.DBHOST
+const DBNAME = process.env.DBNAME
+
+console.log(`Forwarding requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}`)
 
 app.use(express.json())
 
-app.get('/video', (req, res) => {
+const connectDB = async () => {
+    const client = await MongoClient.connect(DBHOST)
+    const db = client.db(DBNAME)
+    return db.collection("videos")
+}
+
+app.get('/video', async (req, res) => {
+
+    if (typeof req.query.id !== 'string') {
+        res.status(404).send("Error, wrong id")
+        return
+    }
+
+    const videoCollection = await connectDB()
+    const videoId = new ObjectId(req.query.id)
+    const videoRecord = await videoCollection.findOne({ _id: videoId })
+    if (!videoRecord) {
+        res.status(404).send("Video not found")
+        return
+    }
+
+    console.log(`Translated id ${videoId} to path ${videoRecord.videoPath}`)
+
     const forwardRequest = request(
         {
             host: VIDEO_STORAGE_HOST,
             port: VIDEO_STORAGE_PORT,
-            path: '/video?path=SampleVideo_1280x720_1mb.mp4',
+            path: `/video?path=${videoRecord.videoPath}`,
             method: 'GET',
             headers: req.headers
         },
--- apps/video-streaming/src/main.ts
+++ apps/video-streaming/src/main.ts
@@ -6,6 +6,13 @@ if (!process.env.PORT) {
 
 const PORT = process.env.PORT
 
-app.listen(PORT, () => {
-    console.log(`Video Streaming service on port ${PORT}`)
+const main = async () => {
+    app.listen(PORT, () => {
+        console.log(`Video Streaming service on port ${PORT}`)
+    })
+}
+
+main().catch(err => {
+    console.error("Microservice failed to start")
+    console.error(err && err.stack || err)
 })
