a6ae3dc91e37da1274ec1c40b9c06b341c309cd9
Myat Kyaw Tun <myatktun2k@gmail.com>
2022-11-23 16:44:10 +0630

    perf(view-history): receive multiple-recipient messages
    src/app/helpers.ts : create an annonymous queue and auto bind a unique
                         id for its name, consume messages from created
                         annonymous queue

--- apps/view-history/src/app/helpers.ts
+++ apps/view-history/src/app/helpers.ts
@@ -29,10 +29,11 @@ const connectRabbit = async () => {
 }
 
 const consumeViewedMessage = async (
+    queue: string,
     videosCollection: Document,
     messageChannel: amqp.Channel
 ) => {
-    await messageChannel.consume("viewed", async (msg) => {
+    await messageChannel.consume(queue, async (msg) => {
         console.log("Received a 'viewed' message")
         if (msg == null) {
             throw new Error("msg is null")
@@ -40,7 +41,7 @@ const consumeViewedMessage = async (
         const parsedMsg = JSON.parse(msg.content.toString())
         await videosCollection.insertOne({ videoPath: parsedMsg.videoPath })
         console.log("Acknowledging message was handled")
-        messageChannel.ack(<amqp.ConsumeMessage>msg)
+        messageChannel.ack(msg)
     })
 }
 
@@ -61,8 +62,12 @@ export const startMessageQueue = async (dbCon: Promise<Db>) => {
     }
 
     const messageChannel = await messageConnection.createChannel()
-    await messageChannel.assertQueue("viewed", {})
-    console.log("Asserted that the 'viewed' queue exists")
+    await messageChannel.assertExchange("viewed", "fanout")
+    // "exclusive: true" will make queue auto deallocate when services disconnect
+    const { queue } = await messageChannel.assertQueue("", { exclusive: true })
+    console.log(`Created queue ${queue}, binding it to "viewed" exchange`)
 
-    await consumeViewedMessage(videosCollection, messageChannel)
+    await messageChannel.bindQueue(queue, "viewed", "")
+
+    await consumeViewedMessage(queue, videosCollection, messageChannel)
 }
